2ca2f72bf8df321a13a133b64d4b02d500ee0e4d
foresToTrain <215723396@qq.com>
2016-10-08 17:38:52 +0800

    测试类

---
+++ src/main/java/com/git/test/Test.java
@@ -0,0 +1,13 @@
+package com.git.test;
+
+/**
+ * Created by hongping【hongp@xkeshi.com】 on 2016/10/8.
+ * Description: //模块目的、功能描述
+ */
+public class Test {
+
+    @org.junit.Test
+    public void test() {
+        System.out.println("地地顶替村奇才顶替枯");
+    }
+}
