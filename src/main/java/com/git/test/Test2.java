fc6d42a4262a9157bea521a0d2fdb5747fcb4fbe
foresToTrain <215723396@qq.com>
2016-10-08 17:51:16 +0800

    测试类2

---
+++ src/main/java/com/git/test/Test2.java
@@ -0,0 +1,13 @@
+package com.git.test;
+
+/**
+ * Created by hongping【hongp@xkeshi.com】 on 2016/10/8.
+ * Description: //模块目的、功能描述
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println("fffffffffff");
+    }
+}
+g
