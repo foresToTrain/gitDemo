6f08096e95dac7cc65e4b2303e850e524295ed32
foresToTrain <215723396@qq.com>
2016-10-09 09:32:00 +0800

    测试类22222

---
+++ src/main/java/com/git/test/Test2222.java
@@ -0,0 +1,13 @@
+package com.git.test;
+
+/**
+ * Created by hongping【hongp@xkeshi.com】 on 2016/10/9.
+ * Description: //模块目的、功能描述
+ */
+public class Test2222 {
+
+
+    public void test(){
+
+    }
+}
